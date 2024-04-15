f1dbffd6e37f1703722abdb412075debd0a26e59
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-15 19:51:55 +0200

    add: JSnack1 - js

--- Snack_1/README.md
+++ Snack_1/README.md
@@ -1,5 +1,34 @@
+# JSnack 1 - es6
 
-Snack 1
+## Text of the exercise
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+## Solution
+
+### FUNCTIONS
+
+
+
+### LOGICAL EXECUTION
+
+- [x] Define *constant* for array of objects. (arrayBikesFeatures)
+>
+- [x] Define *variable* for model of lighter bike. (lighterBikeModel)
+>
+- [x] Define *variable* for weight of lighter bike. (lighterBikeWeight)
+>
+- [x] **FOR EACH** object element of the array (arrayBikesFeatures)
+
+    - [x] Define *constant* for array element attributes. {weight, model}
+
+    - [x] **IF** (lighterBikeWeight === 0) *OR* (lighterBikeWeight > weight)
+
+        - [x] Set (lighterBikeWeight = weight).
+
+        - [x] Set (lighterBikeModel = model).
+
+### OUTPUT
+
+- [x] Print in the body DOM element the lighter model.
---
+++ Snack_1/js/script.js
@@ -0,0 +1,83 @@
+// Define *constant* for array of objects {bikes features}.
+arrayBikesFeatures = [
+    {
+        model: "Trek FX 1",
+        weight: 11,
+    },
+    {
+        model: "Specialized irrus  3.0",
+        weight: 12.5,
+    },
+    {
+        model: "Giant Escap 3",
+        weight: 12,
+    },
+    {
+        model: "Cannondale ick 4",
+        weight: 10.5,
+    },
+    {
+        model: "Scott Sub C ss 40",
+        weight: 13.5,
+    },
+    {
+        model: "Fuji Absolu 1.9",
+        weight: 11.8,
+    },
+    {
+        model: "Diamondbackrace 2",
+        weight: 11.2,
+    },
+    {
+        model: "Raleigh Cadt 2",
+        weight: 12.3,
+    },
+    {
+        model: "GT Transeo 0",
+        weight: 11.5,
+    },
+    {
+        model: "Marin Fairf 1",
+        weight: 10.7,
+    },
+];
+
+// Define *variable* for model of lighter bike.
+let lighterBikeModel = "";
+
+console.log("lighterBikeModel", lighterBikeModel);
+
+// Define *variable* for weight of lighter bike.
+let lighterBikeWeight = 0;
+
+console.log("lighterBikeWeight", lighterBikeWeight);
+
+// Define *variable* for weight of lighter bike.
+arrayBikesFeatures.forEach((element) => { 
+
+    // Define *constant* for array element attributes.
+    const {weight, model} = element;
+
+    console.log("current weight", weight);
+
+    if ((lighterBikeWeight === 0) || (lighterBikeWeight > weight)) {
+
+        lighterBikeWeight = weight;
+
+        lighterBikeModel = model;
+
+    };
+
+    console.log("lighterBikeWeight", lighterBikeWeight);
+
+    console.log("lighterBikeModel", lighterBikeModel);
+
+});
+
+document.querySelector("body")
+.innerHTML =
+`
+    <p>
+        The lighter bike is: ${lighterBikeModel}
+    </p>
+`;
